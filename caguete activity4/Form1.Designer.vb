798ce7440b5c7a4ad5c2da843b6daabe5106ffa0
STC <STC@DESKTOP-4AAGR7G>
2024-02-08 15:05:01 +0800

    Add project files.

---
+++ caguete activity4/Form1.Designer.vb
@@ -0,0 +1,106 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        firstnum = New TextBox()
+        secondnum = New TextBox()
+        Label2 = New Label()
+        Button1 = New Button()
+        lblResult = New Label()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(59, 43)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(72, 15)
+        Label1.TabIndex = 0
+        Label1.Text = "firstnumber:"
+        ' 
+        ' firstnum
+        ' 
+        firstnum.Location = New Point(137, 43)
+        firstnum.Name = "firstnum"
+        firstnum.Size = New Size(100, 23)
+        firstnum.TabIndex = 1
+        ' 
+        ' secondnum
+        ' 
+        secondnum.Location = New Point(137, 83)
+        secondnum.Name = "secondnum"
+        secondnum.Size = New Size(100, 23)
+        secondnum.TabIndex = 2
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(44, 86)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(87, 15)
+        Label2.TabIndex = 3
+        Label2.Text = "secondnumber"
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(59, 126)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(75, 23)
+        Button1.TabIndex = 4
+        Button1.Text = "division"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' lblResult
+        ' 
+        lblResult.AutoSize = True
+        lblResult.Location = New Point(70, 176)
+        lblResult.Name = "lblResult"
+        lblResult.Size = New Size(36, 15)
+        lblResult.TabIndex = 5
+        lblResult.Text = "result"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(lblResult)
+        Controls.Add(Button1)
+        Controls.Add(Label2)
+        Controls.Add(secondnum)
+        Controls.Add(firstnum)
+        Controls.Add(Label1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents firstnum As TextBox
+    Friend WithEvents secondnum As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents lblResult As Label
+End Class
---
+++ caguete activity4/Form1.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim a As Integer
+        Dim b As Integer
+        a = firstnum.Text
+        b = secondnum.Text
+        lblResult.Text = a / b
+    End Sub
+End Class
